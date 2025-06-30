882d29dbedfd573164d560278cfebca3a5882d43
Jonathan Fin <finjonathan@gmail.com>
2025-06-30 16:38:45 +0200

    Baseline

--- LM/part_A/main.py
+++ LM/part_A/main.py
@@ -2,8 +2,97 @@
 # Please write your fuctions or classes in the functions.py
 
 # Import everything from functions.py file
+from functools import partial
+from torch.utils.data import DataLoader
+
 from functions import *
+from utils import *
+from model import LM_RNN
+import torch.optim as optim
+
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import copy
+import numpy as np
 
 if __name__ == "__main__":
     #Wrtite the code to load the datasets and to run your functions
+    train_raw = read_file("dataset/PennTreeBank/ptb.train.txt")
+    dev_raw = read_file("dataset/PennTreeBank/ptb.valid.txt")
+    test_raw = read_file("dataset/PennTreeBank/ptb.test.txt")
+
+    print("Read files")
+
+    # Vocab is computed only on training set 
+    # We add two special tokens end of sentence and padding 
+    lang = Lang(train_raw, ["<pad>", "<eos>"])
+
+    train_dataset = PennTreeBank(train_raw, lang)
+    dev_dataset = PennTreeBank(dev_raw, lang)
+    test_dataset = PennTreeBank(test_raw, lang)
+
+    print("PennTreeBank")
+    DEVICE = 'cuda' 
+
+    # Dataloader instantiation
+    # You can reduce the batch_size if the GPU memory is not enough
+    train_loader = DataLoader(train_dataset, batch_size=64, collate_fn=partial(collate_fn, pad_token=lang.word2id["<pad>"], device=DEVICE),  shuffle=True)
+    dev_loader = DataLoader(dev_dataset, batch_size=128, collate_fn=partial(collate_fn, pad_token=lang.word2id["<pad>"], device=DEVICE))
+    test_loader = DataLoader(test_dataset, batch_size=128, collate_fn=partial(collate_fn, pad_token=lang.word2id["<pad>"], device=DEVICE))
+
+    print("Data loader")
+
+
+    # Experiment also with a smaller or bigger model by changing hid and emb sizes 
+    # A large model tends to overfit
+    hid_size = 200
+    emb_size = 300
+
+    # Don't forget to experiment with a lower training batch size
+    # Increasing the back propagation steps can be seen as a regularization step
+
+    # With SGD try with an higher learning rate (> 1 for instance)
+    lr = 0.0001 # This is definitely not good for SGD
+    clip = 5 # Clip the gradient
+
+    vocab_len = len(lang.word2id)
+
+    model = LM_RNN(emb_size, hid_size, vocab_len, pad_index=lang.word2id["<pad>"]).to(DEVICE)
+    model.apply(init_weights)
+
+    optimizer = optim.SGD(model.parameters(), lr=lr)
+    criterion_train = nn.CrossEntropyLoss(ignore_index=lang.word2id["<pad>"])
+    criterion_eval = nn.CrossEntropyLoss(ignore_index=lang.word2id["<pad>"], reduction='sum')
+
+    n_epochs = 10
+    patience = 3
+    losses_train = []
+    losses_dev = []
+    sampled_epochs = []
+    best_ppl = math.inf
+    best_model = None
+    pbar = tqdm(range(1,n_epochs))
+    #If the PPL is too high try to change the learning rate
+    for epoch in pbar:
+        loss = train_loop(train_loader, optimizer, criterion_train, model, clip)    
+        if epoch % 1 == 0:
+            sampled_epochs.append(epoch)
+            losses_train.append(np.asarray(loss).mean())
+            ppl_dev, loss_dev = eval_loop(dev_loader, criterion_eval, model)
+            losses_dev.append(np.asarray(loss_dev).mean())
+            pbar.set_description("PPL: %f" % ppl_dev)
+            if  ppl_dev < best_ppl: # the lower, the better
+                best_ppl = ppl_dev
+                best_model = copy.deepcopy(model).to('cpu')
+                patience = 3
+            else:
+                patience -= 1
+                
+            if patience <= 0: # Early stopping with patience
+                break # Not nice but it keeps the code clean
+
+    best_model.to(DEVICE)
+    final_ppl,  _ = eval_loop(test_loader, criterion_eval, best_model)    
+    print('Test ppl: ', final_ppl)
+
     # Print the results
